df5cecf9087233476af2309b2ebfa06ede3e59fc
web-barina <why37mr70@gmail.com>
2024-05-09 15:33:00 +0900

    05/09-15:32

--- src/js/script.js
+++ src/js/script.js
@@ -12,7 +12,13 @@ jQuery(function ($) {
       $(this).addClass("active");
     }
   }); //ハンバーガーメニュー閉じタグ
-
+  //768px以上でドロワーを非表示にする
+  $(window).on("resize", function () {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+      $("#hamburger").removeClass("active");
+      $(".sp-nav").fadeOut();
+    }
+  });
   /*****
     FV-swiper
     *****/
@@ -28,24 +34,29 @@ jQuery(function ($) {
   /******************
    * campaign-swiper*
    ******************/
-  var campaignSwiper = new Swiper("#campaignSwiper", {
+  var topCampaignSwiper = new Swiper("#topCampaignSwiper", {
     loop: true,
-    slidesPerView: 1.2,
+    slidesPerView: 1.24,
+    spaceBetween: 24,
     breakpoints: {
+      500: {
+        slidesPerView: 1.8,
+        spaceBetween: 24,
+      },
       768: {
-        slidesPerView: 2.5,
+        slidesPerView: 2.8,
+        spaceBetween: 40,
       },
       1300: {
         slidesPerView: 3.3,
+        spaceBetween: 40,
       },
     },
-    spaceBetween: 40,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
-
   /***************
     カラーボックス
     **************/
@@ -80,44 +91,37 @@ jQuery(function ($) {
    *******************/
   //スクロールした際の動きを関数でまとめる
   function PageTopAnime() {
-    var scroll = $(window).scrollTop(); //スクロール値を取得
+    var scroll = $(window).scrollTop();
     if (scroll >= 300) {
-      //〇pxスクロールしたら
       $("#scroll-top").removeClass("DownMove");
       $("#scroll-top").addClass("UpMove");
     } else {
-      //それ以外は
       if ($("#scroll-top").hasClass("UpMove")) {
-        //UpMoveというクラス名が既に付与されていたら
         $("#scroll-top").removeClass("UpMove");
         $("#scroll-top").addClass("DownMove");
       }
     }
 
-    var wH = window.innerHeight; //画面の高さを取得
-    var footerPos = $("#footer").offset().top; //footerの位置を取得
+    var wH = window.innerHeight;
+    var footerPos = $("#footer").offset().top;
     if (scroll + wH >= footerPos + 10) {
       var pos = scroll + wH - footerPos + 16; //スクロールの値＋画面の高さからfooterの位置＋16pxを引いた場所を取得し
-      $("#scroll-top").css("bottom", pos); //#scroll-topに上記の値をCSSのbottomに直接指定してフッター手前で止まるようにする
+      $("#scroll-top").css("bottom", pos);
     } else {
       //それ以外は
       if ($("#scroll-top").hasClass("UpMove")) {
-        //UpMoveというクラス名がついていたら
-        $("#scroll-top").css("bottom", "10px"); // 下から10pxの位置にページリンクを指定
+        $("#scroll-top").css("bottom", "10px");
       }
     }
   }
-
   // 画面をスクロールをしたら動かしたい場合の記述
   $(window).scroll(function () {
-    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+    PageTopAnime();
   });
-
   // ページが読み込まれたらすぐに動かしたい場合の記述
   $(window).on("load", function () {
-    PageTopAnime(); /* スクロールした際の動きの関数を呼ぶ*/
+    PageTopAnime();
   });
-
   // #scroll-topをクリックした際の設定
   $("#scroll-top").click(function () {
     $("body,html").animate(
@@ -125,7 +129,7 @@ jQuery(function ($) {
         scrollTop: 0, //ページトップまでスクロール
       },
       500
-    ); //ページトップスクロールの速さ。数字が大きいほど遅くなる
+    ); //ページトップスクロールの速さ。
     return false; //リンク自体の無効化
   });
 });
