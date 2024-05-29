f02a1642286d2f8219f767b2dcf1a05f0038383c
web-barina <why37mr70@gmail.com>
2024-05-29 13:29:29 +0900

    05/29-13:29

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -2,5 +2,5 @@
 
 /*document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
-  gsap.fromTo(".js-loading__start", { opacity: 1 }, { opacity: 0, delay: 2, duration: 3 });
+  gsap.fromTo(".js-loading__start", { opacity: 1 }, { opacity: 0, delay: 1, duration: 1 });
 });*/
