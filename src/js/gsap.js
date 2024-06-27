2cfb10917d42d7f7b95dd9b47e53f7d426d2f240
web-barina <why37mr70@gmail.com>
2024-06-27 11:00:01 +0900

    06/27-11:00

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,24 +1,37 @@
+//jQuery(function ($) {
 "use strict";
 const tl = gsap.timeline();
-gsap.fromTo(
-  ".top-js-loading__start",
-  { opacity: 1 },
+
+tl.fromTo(
+  ".js-loading__start",
   {
+    opacity: 1,
+  },
+  {
+    delay: 1,
+    duration: 1.5,
     autoAlpha: 0,
-    delay: 1,
-    duration: 1,
   }
-);
-fromTo(
-  ".top-js-loading__last",
-  { opacity: 1 },
-  {
-    delay: 1,
-    duration: 1,
-    opacity: 0,
-    onComplete: function () {
-      // アニメーションが終了したら要素を削除する
-      document.querySelector(".top-js-loading__last").remove();
+)
+  .fromTo(
+    ".js-loading__middle",
+    {
+      opacity: 1,
     },
-  }
-);
+    {
+      delay: 1,
+      duration: 1.5,
+      autoAlpha: 0,
+    }
+  )
+  .fromTo(
+    ".js-loading__last",
+    {
+      opacity: 1,
+    },
+    {
+      delay: 1,
+      duration: 1,
+      autoAlpha: 0,
+    }
+  );
