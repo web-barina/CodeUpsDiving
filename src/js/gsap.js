181425315dc27273fb2ab29b914697bc20f149b5
web-barina <why37mr70@gmail.com>
2024-07-11 01:04:28 +0900

    07/11

--- src/js/gsap.js
+++ src/js/gsap.js
@@ -1,5 +1,6 @@
 "use strict";
 const tl = gsap.timeline();
+const screenHeight = window.innerHeight;
 
 tl.fromTo(
   ".js-loading__start",
@@ -8,17 +9,21 @@ tl.fromTo(
   },
   {
     delay: 1,
-    duration: 2,
-    autoAlpha: 0,
-  }
-).fromTo(
-  ".js-loading__last",
-  {
-    opacity: 1,
-  },
-  {
-    delay: 1,
     duration: 1,
     autoAlpha: 0,
   }
-);
+)
+  .fromTo(".js-loading__left", { y: screenHeight, opacity: 1 }, { y: 0, opacity: 1, duration: 1 })
+  .fromTo(".js-loading__right", { y: screenHeight + 80, opacity: 1 }, { y: 0, opacity: 1, duration: 1 })
+  .to(".js-loading__middle", { opacity: 0, duration: 1, autoAlpha: 0 })
+  .fromTo(
+    ".js-loading__last",
+    {
+      opacity: 1,
+    },
+    {
+      delay: 1,
+      duration: 1,
+      autoAlpha: 0,
+    }
+  );
